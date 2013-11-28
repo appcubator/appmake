ccf241f51faa9d152846c54c08f78fdb1abe0ab0
Karan Sikka <kssworld93@gmail.com>
2013-11-28 17:04:27 -0500

    yo

--- appmake.js
+++ appmake.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+var parser = require('./frontend/parser');
 var validator = require('./frontend/validator');
 var expander = require('./frontend/expander');
 var writer = require('./backend/writer');
@@ -7,13 +8,40 @@ var path = require('path');
 
 if (require.main === module) {
     var USAGE = '\n  Usage:\n\n'+
+                '    ./appmake.js parse <src_dir> <json_file_out>\n\n' +
+                '       Parses the source code files at the src_dir, and outputs\n' +
+                '           an app json at json_file_out.\n\n' +
+
                 '    ./appmake.js compile <json_file> <output_dir>\n\n' +
                 '       Compiles the app given by the json_file, and outputs\n' +
                 '           a node app at the output_dir.\n\n' +
+
                 '    ./appmake.js deploy <app_dir>\n\n' +
                 '       Deploys the app in app_dir to the Appcubator cloud.\n\n';
 
+    if (process.argv.length < 3) {
+        process.stdout.write(USAGE);
+        process.exit();
+    }
+
     switch (process.argv[2]) {
+
+        case "parse":
+            if (process.argv.length < 5) {
+                process.stdout.write("Not enough arguments for parse:\n");
+                process.stdout.write(USAGE);
+                break;
+            }
+            var srcDir = process.argv[3];
+            var destJsonPath = path.resolve(process.argv[4]);
+            if (fs.existsSync(destJsonPath)) {
+                process.stdout.write("The path \""+destJsonPath+"\" already exists.");
+                process.exit(1);
+            }
+
+            var app = parser.parseDir(srcDir);
+
+            fs.writeFileSync(destJsonPath, JSON.stringify(app, null, 2));
 
         case "compile":
             if (process.argv.length < 5) {
---
+++ frontend/parser.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+
+exports.parseDir = function (dirPath) {
+    console.log('Parser not yet implemented. Returning dummy json.');
+    return {};
+};
