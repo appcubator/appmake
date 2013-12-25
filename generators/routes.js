338960fad1b518d8a5c021e290ae8e0a394a139f
Karan Sikka <kssworld93@gmail.com>
2013-12-24 21:54:53 -0500

    works

--- generators/routes.js
+++ generators/routes.js
@@ -16,4 +16,26 @@ generators.push({
                        "}"}
 });
 
+generators.push({
+    name: 'apiroute',
+    version: '0.1',
+    code: function(data, templates){
+        // TODO allow custom url and method.
+        var url = '/api/' + data.modelName + '/' + data.methodName;
+        var route = {
+            "method": "POST",
+            "pattern": url,
+            "code": templates.code(data)
+        };
+        return route;
+    },
+    templates: {'code': "function (req, res) {"+"\n"+
+                "    var <%= modelName %> = require('./models/<%= modelName %>').<%= modelName %>;"+"\n"+
+                "    var whenDone = function(e, d) { res.send({error:e, data:d}); };"+"\n"+
+                "    var args = req.body;"+"\n"+
+                "    args.push(whenDone);"+"\n"+
+                "    <%= modelName %>.<%= methodName %>.apply(<%= modelName %>, args);"+"\n"+
+                "}"}
+});
+
 exports.generators = generators;
