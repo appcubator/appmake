94e9222605f2ab7b0023a3e44b9afc2f9b74f264
Karan Sikka <kssworld93@gmail.com>
2013-12-29 05:04:19 -0500

    mongoose user model

---
+++ tests/flickrpickr/models/User.js
@@ -0,0 +1,105 @@
+var fields = {
+  name: { type: 'String', default: '' },
+  email: { type: 'String', default: '' },
+  username: { type: 'String', default: '' },
+  hashed_password: { type: 'String', default: '' },
+  salt: { type: 'String', default: '' }
+};
+
+var instancemethods = [];
+
+instancemethods.push({
+  name: 'authenticate',
+  code: function(plainText) {
+  /**
+   * Authenticate by checking the hashed password and provided password
+   *
+   * @param {String} plainText
+   * @return {Boolean}
+   * @api private
+   */
+    return this.encryptPassword(plainText) === this.hashed_password;
+  }
+});
+
+instancemethods.push({
+  name: makeSalt,
+  code: function() {
+  /**
+   * Create password salt
+   *
+   * @return {String}
+   * @api private
+   */
+
+    /* Then to regenerate password, use:
+        this.salt = this.makeSalt()
+        this.hashed_password = this.encryptPassword(password)
+    */
+    return Math.round((new Date().valueOf() * Math.random())) + '';
+  }
+});
+
+instancemethods.push({
+  name: encryptPassword,
+  code: function (password) {
+  /**
+   * Encrypt password
+   *
+   * @param {String} password
+   * @return {String}
+   * @api private
+   */
+    var crypto = require('crypto');
+    if (!password) return '';
+    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+  }
+});
+
+
+instancemethods.push({
+  name: resetToken,
+  code: function (token, cb) {
+  /**
+   * Reset auth token
+   *
+   * @param {String} token
+   * @param {Function} cb
+   * @api private
+   */
+    var self = this;
+    var crypto = require('crypto');
+    crypto.randomBytes(48, function(ex, buf) {
+      self[token] = buf.toString('hex');
+      if (cb) cb();
+    });
+  }
+});
+
+var schemaMods = [];
+schemaMods.push(function (schema) {
+  schema.path('name').validate(function (name) {
+    return name.trim().length > 0;
+  }, 'Please provide a valid name');
+});
+
+schemaMods.push(function (schema) {
+  schema.path('email').validate(function (email) {
+    return email.trim().length > 0;
+  }, 'Please provide a valid email');
+});
+
+schemaMods.push(function (schema) {
+  schema.path('hashed_password').validate(function (hashed_password) {
+    return hashed_password.length > 0;
+  }, 'Please provide a password');
+});
+
+
+var staticmethods = [];
+
+var model = {fields: fields,
+             instancemethods: instancemethods,
+             staticmethods: staticmethods,
+             schemaMods: schemaMods};
+
