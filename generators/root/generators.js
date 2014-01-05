a5cde91259cccc6c78f0f84fe8ac65ea3e52ebcf
Karan Sikka <kssworld93@gmail.com>
2014-01-05 09:22:05 -0500

    old html node template

--- generators/root/generators.js
+++ generators/root/generators.js
@@ -1,4 +1,5 @@
 exports.routes = require('./routes.js').generators;
 exports.templates = require('./templates.js').generators;
+exports.uielements = require('./uielements.js').generators;
 exports.models = require('./models.js').generators;
 exports.app = require('./app.js').generators;
