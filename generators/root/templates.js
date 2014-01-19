c59b420a233d8e21903f8a10ea02f37f52b1d528
Ilter Canberk <ilteriscanberk@gmail.com>
2014-01-19 15:53:03 -0500

    fix

--- generators/root/templates.js
+++ generators/root/templates.js
@@ -438,7 +438,7 @@ generators.push({
         return str;
     },
     templates: {
-            "hero": '<div class="jumbotron"><div class="container ycol"><%= col0 %></div></div>',
+            "12": '<div class="jumbotron"><div class="container ycol"><%= col0 %></div></div>',
             "3-3-3-3" : [
                     '<div class="container">',
                         '<div class="row">',
