113e32643cc07237a1aa18e87d9abd95223696e7
Karan Sikka <kssworld93@gmail.com>
2014-02-02 13:34:37 -0500

    forgot to add this

---
+++ client/app/scripts/generator.js
@@ -0,0 +1,37 @@
+Generator = (function(_){
+    'use strict';
+
+    var Generator = function() {
+        var Vm = function() {
+
+            this.runCode = function(code, globals) {
+                var templates = globals.templates;
+                var data = globals.data;
+                var expand = globals.expand;
+                return eval(code);
+            };
+
+        };
+
+        var VM = new Vm();
+        this.expander = expanderfactory(function(code, globals) {
+            return VM.runCode(code, globals);
+        });
+
+    };
+
+    Generator.prototype.generate = function(generatorPath, data) {
+
+        return this.expander.expand([], {generate: generatorPath, data: data});
+    };
+
+    /**
+    Generator.prototype.getGenerator = function(generatorPath) {
+
+        return this.expander.findGenData(v1.currentApp.model.serialize().generators, this.expander.parseGenID(generatorPath));
+
+    };
+    */
+
+    return Generator;
+})(_);
