3c389c0bba5b8279c9bc1d4abdad3fcbabc8ef55
Karan Sikka <kssworld93@gmail.com>
2014-03-10 01:26:01 -0700

    uielements displayProps

--- generators/crud/generators.js
+++ generators/crud/generators.js
@@ -1,3 +1,7 @@
 exports.uielements = require('./uielements.js').generators;
 exports.model_methods = require('./model_methods.js').generators;
-exports.metadata = { name: 'crud' };
+exports.metadata = {
+    name: 'crud',
+    displayName: 'Basic Database Stuff',
+    description: 'Basic database stuff'
+};
