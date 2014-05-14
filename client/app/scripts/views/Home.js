5bb5e4fea55ab6e2500ea45a53e44e449f6c414e
Ilter Canberk <iltercanberk@gmail.com>
2014-05-14 00:02:05 -0400

    Search stuff

--- client/app/scripts/views/Home.js
+++ client/app/scripts/views/Home.js
@@ -9,14 +9,59 @@ define([
     'use strict';
 
     var HomeView = Backbone.View.extend({
+        
         template: JST['app/scripts/templates/Home.ejs'],
+        
+        events: {
+            "#pluging-search-form submit" : "searchSubmitted"
+        },
+
         render: function(){
+        
         	var result = this.template({});
         	this.$el.html( result );
+        
         },
+        
         initialize: function(){
+            _.bindAll(this);
             this.render();
+        },
+
+        searchSubmitted: function(e) {
+            e.preventDefault();
+            var self = this;
+            var val = $('#repoSearchInput').val();
+            $.ajax({
+                type: "GET",
+                url: '/plugins/search?q='+val,
+                statusCode: {
+                    200: function(data) {
+                        self.renderList(data)
+                    },
+                    400: function(jqxhr) {
+                        self.renderError();
+                    },
+                },
+                dataType: "JSON"
+            });
+        },
+
+        renderList: function(list) {
+            console.log(list);
+            var $list = $('#search-list');
+            $list.html('');
+
+            _.each(function(val, ind) {
+                $list.append(val.name);
+            })
+        },
+
+        renderError: function() {
+            $list.html('Error.');
         }
+
+
     });
 
     return HomeView;
